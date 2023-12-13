d4dced2445f50ec2ef21985227b56496d3896ac6
Bogdan <122648041+nisianot@users.noreply.github.com>
2023-12-13 10:48:53 +0300

    Create example.py

---
+++ example.py
@@ -0,0 +1,7 @@
+from loading_module import print_string_anim, simulate_loading
+
+# Example of using the print_string_anim function
+print_string_anim("Hello, World!", 0.1)
+
+# Example of using the simulate_loading function
+simulate_loading(3)
